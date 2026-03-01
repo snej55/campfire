8992a9f57a48f61bcf92788c2c5aaa326418b661
Jens Kromdijk <jenskromdijk@gmail.com>
2026-03-01 17:17:26 +0000

    Add nautilus

--- main.py
+++ main.py
@@ -59,7 +59,9 @@ class App:
             "background": load_image("background.png"),
             "player/bubble": load_animation("player/bubble.png", 12, 13, 2),
             "pufferfish": load_animation("pufferfish.png", 16, 13, 3),
-            "pin": load_image("pin.png")
+            "nautilus": load_animation("nautilus.png", 28, 17, 6),
+            "pin": load_image("pin.png"),
+            "shell": load_image("shell.png")
         }
 
         surf = pygame.Surface(self.assets["background"].get_size())
@@ -79,6 +81,13 @@ class App:
                 del self.tile_map.tile_map[loc]
         self.pins = []
 
+        self.nautilus = []
+        for loc in self.tile_map.tile_map.copy():
+            if self.tile_map.tile_map[loc]["type"] == "nautilus":
+                self.nautilus.append(Nautilus(self, [self.tile_map.tile_map[loc]["pos"][0] * TILE_SIZE, self.tile_map.tile_map[loc]["pos"][1] * TILE_SIZE], self.assets["nautilus"]))
+                del self.tile_map.tile_map[loc]
+        self.shells = []
+
         self.player = Player(self, [6, 7], [20, 15])
         self.screen_shake = 0
 
@@ -97,6 +106,8 @@ class App:
         
         for fish in self.pufferfish:
             fish.update(self.dt, self.player)
+        for fish in self.nautilus:
+            fish.update(self.dt, self.player)
 
         # do the rendering
         screen_shake_offset = (random.random() * self.screen_shake - self.screen_shake / 2, random.random() * self.screen_shake - self.screen_shake / 2)
@@ -104,16 +115,30 @@ class App:
         self.screen_shake = max(0, self.screen_shake - 1 * self.dt)
         self.screen.blit(pygame.transform.scale(self.assets["background"], self.screen.get_size()), (0, 0))
         for i, pin in sorted(enumerate(self.pins), reverse=True):
-            pin.update(self.dt, self.tile_map) 
+            pin.update(self.dt, self.tile_map)
+            if pin.get_rect().colliderect(self.player.get_rect()):
+                self.player.die()
+                pin.kill = True
             if pin.kill:
                 self.pins.pop(i)
             else:
                 pin.draw(self.screen, render_scroll)
+        for i, shell in sorted(enumerate(self.shells), reverse=True):
+            shell.update(self.dt, self.tile_map)
+            if shell.get_rect().colliderect(self.player.get_rect()):
+                self.player.die()
+                shell.kill = True
+            if shell.kill:
+                self.shells.pop(i)
+            else:
+                shell.draw(self.screen, render_scroll)
 
         self.tile_map.draw_decor(self.screen, render_scroll)
         if self.player.ad > self.player.death_time:
             self.player.draw(self.screen, render_scroll)
         for fish in self.pufferfish:
+            fish.draw(self.screen, render_scroll)
+        for fish in self.nautilus:
             fish.draw(self.screen, render_scroll)
         self.tile_map.draw(self.screen, render_scroll)
 
